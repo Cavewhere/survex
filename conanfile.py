05840b471526c737702da53678d76763c92c738d
Philip Schuchardt <vpicaver@gmail.com>
2023-09-11 23:28:30 -0700

    Updated the versions

--- conanfile.py
+++ conanfile.py
@@ -11,19 +11,20 @@ class SurvexConan(ConanFile):
     version = "1.0"
     settings = "os", "compiler", "build_type", "arch"
     requires = [
-    ("wxwidgets/3.1.4@bincrafters/stable"),
+    ("wxwidgets/3.1.5@bincrafters/stable"),
     ("glew/2.2.0"),
-    ("proj/9.1.0"),
-    ("libtiff/4.5.0"),
-    ("zlib/1.2.13")
+    ("proj/9.2.1"),
+    ("zlib/1.2.13"),
+    ("libtiff/4.0.9"),
     ]
 
-    generators = "cmake_find_package", "cmake_paths"
+    generators = "cmake_find_package", "cmake_paths", "cmake"
 
     def configure(self):
         #This is survex dependancy
-        self.options["wxwidgets"].webview=False
-        self.options["proj"].shared=True
-        self.options["zlib"].shared=True
+        #        self.options["wxwidgets"].webview=False
         self.options["wxwidgets"].shared=True
+        self.options["proj"].shared=True
+        self.options["proj"].with_tiff=False
+        self.options["zlib"].shared=True
 
