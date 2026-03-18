159f7a5b9c7482474770c3b58867da661131184b
Philip Schuchardt <vpicaver@gmail.com>
2026-03-17 19:55:48 -0700

    apple support for gitopt

--- src/getopt.c
+++ src/getopt.c
@@ -67,7 +67,7 @@
 
 /* This needs to come after some library #include
    to get __GNU_LIBRARY__ defined.  */
-#if 1 /* Survex doesn't use getopt() so we don't declare it. */
+#if defined(__APPLE__) /* Survex doesn't use getopt() so we don't declare it. macOS needs stdlib.h included here to avoid conflicting prototypes. */
 /* Don't include stdlib.h for non-GNU C libraries because some of them
    contain conflicting prototypes for getopt.  */
 # include <stdlib.h>
